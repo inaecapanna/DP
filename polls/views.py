d668b2dc37e7838123f3ca8f3d64a3fc97dbb094
Inae <icapannasoriani2gmail.com>
2023-06-20 10:53:31 -0300

    incluir arquivos criados / modificados

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def ultimas_perguntas(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/perguntas.html', context)
