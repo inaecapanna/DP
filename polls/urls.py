f480b7db3eb5a63447bd038a3db8f9fb2c5bccc3
Inae <icapannasoriani2gmail.com>
2023-06-20 11:08:32 -0300

    modificar e adicionar

--- polls/urls.py
+++ polls/urls.py
@@ -1,23 +1,14 @@
 from django.urls import path
 
+# importar da pasta atual o arquivo views.py
 from . import views
 
 urlpatterns = [
-path("", views.index, name="index")
+    path('', views.index, name='index'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao'),
+    path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
 ]
 
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-# ex: /polls/
-path("", views.index, name="index"),
-# ex: /polls/5/
-path("/", views.detail, name="detail"),
-# ex: /polls/5/results/
-path("/results/", views.results, name="results"),
-# ex: /polls/5/vote/
-path("/vote/", views.vote, name="vote"),
-path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
-]
+# .\venv\Scripts\activate.bat
+# python manage.py runserser
--- polls/views.py
+++ polls/views.py
@@ -1,78 +1,27 @@
-from django.http import HttpResponse
-
-def index(request):
-return HttpResponse("Hello, world. You're at the polls index.")
-
-def detail(request, question_id):
-return HttpResponse("Você está olhando para a pergunta %s." % question_id)
-
-
-def results(request, question_id):
-response = "Você está olhando para os resultados da pergunta %s."
-return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-return HttpResponse("Você está votando na pergunta %s." % question_id)
-
-from django.http import HttpResponse
-
-from .models import Question
-
-
-def index(request):
-latest_question_list = Question.objects.order_by("-pub_date")[:5]
-output = ", ".join([q.question_text for q in latest_question_list])
-return HttpResponse(output)
-
-from django.http import HttpResponse
-from django.template import loader
-
-from .models import Question
-
-def index(request):
-latest_question_list = Question.objects.order_by("-pub_date")[:5]
-template = loader.get_template("polls/index.html")
-context = {
-"latest_question_list": latest_question_list,
-}
-return HttpResponse(template.render(context, request))
-
-
 from django.shortcuts import render
+from django.http import HttpResponse
 
-from .models import Question
+# Create your views here.
+from polls.models import Question, Choice
 
-
+# View index ... carregada para alguma rota (caminho)
 def index(request):
-latest_question_list = Question.objects.order_by("-pub_date")[:5]
-context = {"latest_question_list": latest_question_list}
-return render(request, "polls/index.html", context)
+    return HttpResponse('Olá... seja bem vindo a enquete')
 
-from django.http import Http404
-from django.shortcuts import render
+def sobre(request):
+    return HttpResponse('Este é um app de enquete!')
 
-from .models import Question
+def exibe_questao(request, question_id):
+    questao = Question.objects.get(id=question_id)
+    
+    if questao is not None:
+        # questao.question_text
+        return HttpResponse(questao.question_text)
+    
+    return HttpResponse('Não existe questão a exibir')
 
-# ...
-def detail(request, question_id):
-try:
-question = Question.objects.get(pk=question_id)
-except Question.DoesNotExist:
-raise Http404("Question does not exist")
-return render(request, "polls/detail.html", {"question": question})
-
-from django.shortcuts import get_object_or_404, render
-
-from .models import Question
-
-
-# ...
-def detail(request, question_id):
-question = get_object_or_404(Question, pk=question_id)
-return render(request, "polls/detail.html", {"question": question})
 
 def ultimas_perguntas(request):
-latest_question_list = Question.objects.order_by('-pub_date')[:5]
-context = {'latest_question_list': latest_question_list}
-return render(request, 'polls/perguntas.html', context)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/perguntas.html', context)
