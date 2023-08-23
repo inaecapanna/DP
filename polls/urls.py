11cefa6dd91a19538ba5399e4290aa3a79b9024f
Inae Capanna <icapannasoriani@gmail.com>
2023-08-23 09:38:38 -0300

    adicionar

--- polls/urls.py
+++ polls/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('sobre/', views.sobre, name='sobre'),
     path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao'),
     path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
+    path('cadastrar',
+    views.QuestionCreateView.as_view(),
+    name = 'question-create')
 ]
 
 # .\venv\Scripts\activate.bat
